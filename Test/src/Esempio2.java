6efdf0cbb022fe3d00b9aa666578c4e15bd77158
Giuseppe Conza <giuseppeforce36@gmail.com>
2025-02-26 17:54:50 +0100

    zz

---
+++ Test/src/Esempio2.java
@@ -0,0 +1,41 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+
+public class Esempio2 {
+    public static void main(String[] args) throws Exception {
+        String DB_DRIVER = "com.mysql.jdbc.Driver";
+        String DB_URL = "jdbc:mysql://localhost:3306/world";
+        String DB_USERNAME = "root";
+        String DB_PASSWORD = "root";
+        Connection conn = null;
+
+        try {
+            Class.forName(DB_DRIVER);
+
+            conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+
+            if (conn != null){
+                String query = "SELECT * from city limit ?";
+                int limite = 5; // varibile del limit per la query
+
+                PreparedStatement pstmt = conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+                pstmt.setInt(1, limite); // setta il primo "?" a limite
+
+                ResultSet rs = pstmt.executeQuery();
+                while (rs.next()) {
+                    System.out.println(rs.getString("Name"));
+                }
+
+            }
+            else
+                System.out.println("NON CONNESSO");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
