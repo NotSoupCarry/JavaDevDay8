93a61df2f7220e22aea2d2fc1eb8c8000d5f6395
Giuseppe Conza <giuseppeforce36@gmail.com>
2025-02-27 09:25:19 +0100

    nj

--- Test/src/Esempio2.java
+++ Test/src/Esempio2.java
@@ -2,9 +2,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.Statement;
-
 
 public class Esempio2 {
     public static void main(String[] args) throws Exception {
